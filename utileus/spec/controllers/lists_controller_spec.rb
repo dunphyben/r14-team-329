df133ded5f6cd369dd0f2dd85f0efe0ea739debd
Alex Wood <ajwood04@gmail.com>
2014-10-17 20:24:15 -0700

    adds controller, view, model for lists

---
+++ utileus/spec/controllers/lists_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe ListsController do 
+	describe "index" do 
+		before(:each) do 
+			@user = FactoryGirl.create(:user, id: 1)
+			@list = FactoryGirl.create(:list, id: 1, user_id: 1)
+			get :index
+		end
+
+		it "returns http code 200" do
+			expect(response).to have_http_status(200)
+		end
+
+		it "returns a list of a user's lists" do 
+			expect(@user.lists).not_to eq nil
+		end
+	end
+end
