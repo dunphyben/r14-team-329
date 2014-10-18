7dab8ea055e0b79fc04918d57ed5fc5e15d3b963
Isaac Rosenberg <irosenb7@gmail.com>
2014-10-18 10:18:12 -0700

    added bootstrap

--- utileus/config/routes.rb
+++ utileus/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :lists
   get "/auth/:provider/callback" => "users#create"
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
