eab2674c5362bc91df9b261b61e78789b4a03e5a
Alex Wood <ajwood04@gmail.com>
2014-10-18 19:05:19 -0700

    adds seeds.rb

--- utileus/db/seeds.rb
+++ utileus/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(name: 'William Shakespeare', email: "billy@shakes.com", password: "hamlet1996", password_confirmation: "hamlet1996")
+profession = user.professions.create(name: 'Playwright')
+list = user.lists.create(name: 'My Plays')
+apps = list.apps.create([{ name: 'Hamlet', url: "www.example.com/hamlet" }, { name: 'Othello', url: "www.example.com/othello"}, { name: "Romeo & Juliet", url: "www.example.com/rnj" }, { name: "Macbeth", url: "www.example.com/mac" }])
