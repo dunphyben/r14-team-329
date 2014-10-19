58646a278a9df8a842a42047ff3218bbe393947e
Isaac Rosenberg <irosenb7@gmail.com>
2014-10-18 20:39:20 -0700

    Revert "solr working"
    This reverts commit 6894e14e5c33853fca49b4d19ca7e0ab2964358e.

--- utileus/app/models/list.rb
+++ utileus/app/models/list.rb
@@ -3,10 +3,4 @@ class List < ActiveRecord::Base
 	has_many :apps
 	validates :name, presence: true
 	acts_as_votable
-
-  searchable do
-    text :profession do
-      user.professions.map { |profession| profession.name }
-    end
-  end
 end
